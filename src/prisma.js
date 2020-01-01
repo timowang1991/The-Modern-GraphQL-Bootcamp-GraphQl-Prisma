34baf22131104f54a80f2669697c471351471271
王權 <timowang@wangquande-MacBook-Pro.local>
2020-01-01 17:41:52 +0800

    prisma binding query and mutation mess around

--- src/prisma.js
+++ src/prisma.js
@@ -2,6 +2,51 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466'
+    endpoint: 'http://localhost:4466'
 });
 
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log('========= data', JSON.stringify(data[0], null, 4));
+// });
+
+// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
+//     console.log('======== comments', JSON.stringify(data, null, 4));
+// });
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: 'My new GraphQl post is live!',
+//         body: 'you can find the new course here',
+//         published: true,
+//         author: {
+//             connect: {
+//                 id: 'ck4sh9qqk00kn0795xr6e2s2k'
+//             }
+//         }
+//     }
+// }, '{ id title body published }').then((data) => {
+//     console.log(data)
+//     return prisma.query.users(null, '{ id name posts { id title } }');
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
+
+// prisma.mutation
+//     .updatePost(
+//         {
+//             data: {
+//                 body: 'the very new body'
+//             },
+//             where: {
+//                 id: 'ck4v3qztr017e0795b781obsn'
+//             }
+//         },
+//         '{ id title body }'
+//     )
+//     .then((data) => {
+//         console.log(data);
+//         return prisma.query.posts(null, '{ id title body published }');
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
