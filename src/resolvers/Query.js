693d98e6f79d9de214fb3624da8ade7f60c78501
王權 <timowang@wangquande-MacBook-Pro.local>
2020-01-05 21:56:14 +0800

    add published: true query to posts

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -23,22 +23,17 @@ const Query = {
         return prisma.query.users(opArgs, info);
     },
     posts(parents, args, { prisma }, info) {
-        // if (!args.query) {
-        //     return db.posts;
-        // }
-        // return db.posts.filter((post) => {
-        //     return post.title.toLowerCase().includes(args.query.toLowerCase())
-        //         || post.body.toLowerCase().includes(args.query.toLowerCase());
-        // });
-        const opArgs = {};
-        if (args.query) {
-            opArgs.where = {
-                OR: [{
-                    title_contains: args.query
-                }, {
-                    body_contains: args.query
-                }]
+        const opArgs = {
+            where: {
+                published: true
             }
+        };
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
         }
 
         return prisma.query.posts(opArgs, info);
