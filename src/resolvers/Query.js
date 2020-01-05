d03bfd334414962e982e65b99704ccce8b351b7a
王權 <timowang@wangquande-MacBook-Pro.local>
2020-01-05 22:03:49 +0800

    add myPost query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -38,6 +38,27 @@ const Query = {
 
         return prisma.query.posts(opArgs, info);
     },
+    myPosts(parents, args, { prisma, request }, info) {
+        const userId = getUserId(request);
+
+        const opArgs = {
+            where: {
+                author: {
+                    id: userId
+                }
+            }
+        }
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info);
+    },
     comments(parent, args, { prisma }, info) {
         return prisma.query.comments(null, info);
     },
